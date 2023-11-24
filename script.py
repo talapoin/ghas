1fb95319327f31080e65b55020827ea5d3c107ca
Michal Zagraba <michal.zagraba@knowit.eu>
2023-11-24 21:33:13 +0100

    storage account key

--- script.py
+++ script.py
@@ -2,4 +2,5 @@ x = 1
 if x == 1:
     # indented four spaces
     secret="AbuseMe2023"
+    storageaccount='DefaultEndpointsProtocol=https;AccountName=csb1003200092e63f17;AccountKey=HlvFvd4ErbTcD8bi8uCUj801sPQfatlkyykxF+lCEAjYi9AMz7/y0yU7MRsvTPterGaY5RFGduWnqACBNeK7/g==;EndpointSuffix=core.windows.net'
     print(secret)
